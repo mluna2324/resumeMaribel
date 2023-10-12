a842b331a30c5ac8e8a87a51826d70f618d17fd6
mluna2324 <141606181+mluna2324@users.noreply.github.com>
2023-10-12 11:03:29 -0700

    Resume problem solve

---
+++ script.js
@@ -0,0 +1,77 @@
+function randomlinks(){
+    var urls = new Array();
+    urls[0]="https://youtu.be/Wr6v4deKarI";
+    urls[1]="https://youtu.be/XMmdCTdO_pU";
+    urls[2]="https://youtu.be/FK3BJXIJerI";
+    urls[4]="https://youtu.be/x72YZ2A6omk";
+    urls[5]="https://youtu.be/zaBtpDFPcGg";
+    urls[6]="https://youtu.be/1IW7sH1l-Qg";
+    urls[7]="https://youtu.be/h3MxEHQk644";
+    urls[8]="https://youtu.be/9aFV7aZsXgo";
+    urls[9]="https://www.yyyyyyy.info/";
+    urls[10]="http://sentimentalcorp.org/";
+    urls[11]="https://youtu.be/moFB-j5iY2E";
+    urls[12]="http://111111111111111111111111111111111111111111111111111111111111.com/";
+    urls[13]="https://lomando.com/okjonasawuagukobeng.html";
+    urls[14]="https://youtu.be/m3WRLhQuk2U";
+    urls[15]="https://youtu.be/NZh5YxDpuK4";
+    urls[16]="https://youtu.be/648UkmmTG5w";
+    urls[17]="https://youtu.be/pbfXHDBsAw4";
+    urls[18]="https://youtu.be/ZexwGDjxrpk";
+    urls[19]="https://neave.tv/";
+    urls[20]="https://illuminatiorder.info/ac/thaumaturgy/";
+    urls[21]="https://youtu.be/EmHYhDXz-Zs";
+    urls[22]="https://youtu.be/0SOyxasaG20";
+    urls[23]="https://youtu.be/MkMug4zVWfM";
+    urls[24]="https://youtu.be/jk9NLUyYEpw";
+    urls[25]="https://www.youtube.com/live/WvyvwlowHWM?feature=share";
+    urls[26]="https://youtu.be/uY6U_dfHdMM";
+    urls[27]="https://existentialcrisis.com/";
+    urls[28]="https://youtu.be/LQ_e84bk7V8";
+    urls[29]="https://youtu.be/fe33-940ePQ";
+    urls[30]="https://youtu.be/F13Zx-L5XLA";
+    urls[31]="https://youtu.be/IhGDxw4DGHY";
+    urls[32]="https://youtu.be/bB08ai6ub7w";
+    urls[33]="https://youtu.be/EdnHO1wTaPU";
+    urls[34]="https://youtu.be/ozCjlwTwR4Y";
+    urls[35]="https://youtu.be/a1KBb9mTgck";
+    urls[36]="https://youtu.be/tqU3tsZ-Grk";
+    urls[37]="https://youtu.be/eQxveh2d-Js";
+    urls[38]="https://youtu.be/HHOn8u-c2wk";
+    urls[39]="https://youtu.be/nEnklxGAmak";
+    urls[40]="https://youtu.be/_W1P7AvV17w";
+    urls[41]="https://www.jpegmafia.net/";
+    var random = Math.floor(Math.random()*urls.length);
+    window.open(urls[random], '_blank')
+    }
+    function show(){
+      var x = document.getElementById("help");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    
+    function skillShow(){
+      var x = document.getElementById("code");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showFluency(){
+      var x = document.getElementById("eng");
+      if (x.style.display === "none") {
+        x.style.display = "inline-block";
+      } else {
+        x.style.display = "none";
+      }
+      var y = document.getElementById("esp");
+      if (y.style.display === "none") {
+        y.style.display = "inline-block";
+      } else {
+        y.style.display = "none";
+      }
+    }
